638b7c61e57b4034842b4a2760a046da42a30e87
root <root@pranav.localdomain>
2018-06-20 12:53:34 +0530

    7

--- src/main/java/com/miniproj/blog/Controller/HelloController.java
+++ src/main/java/com/miniproj/blog/Controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 	
 	@GetMapping("/hello")
 	public String printhelloworld() {
-		return "hello-Pranavadurai version 6 ";
+		return "hello-Pranavadurai version 7 ";
 	}
 }
